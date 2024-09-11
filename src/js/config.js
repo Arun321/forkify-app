ad4411b043397af025aa19f83aa245357eabaf77
Arun321 <arundharmaraj78@gmail.com>
2024-09-11 23:24:18 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = '63b6a726-ad00-4b26-a718-af2abbd6dcb1';
+export const MODAL_CLOSE_SEC = 2.5;
